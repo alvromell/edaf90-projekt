15907cc71c9cc5431ad3ecbe4855a238539ac434
teklabel <calle.janlenstenberg@gmail.com>
2021-03-02 10:36:53 +0100

    tog bort default html, installera jquery, la till canvasjs i assets, la till implicityany:false i tsconfig, skapa en stock component som renderar en chart

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StockComponent } from './stock/stock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/stock/stock.component.ts
@@ -0,0 +1,121 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import * as CanvasJS from '../../assets/canvasjs-stock-1.2.10/canvasjs.stock.min';
+
+@Component({
+  selector: 'app-stock',
+  templateUrl: './stock.component.html',
+  styleUrls: ['./stock.component.css']
+})
+export class StockComponent implements OnInit {
+
+  constructor() { }
+  addSymbols(e){
+    var suffixes = ["", "K", "M", "B"];
+    var order = Math.max(Math.floor(Math.log(e.value) / Math.log(1000)), 0);
+    if(order > suffixes.length - 1)
+      order = suffixes.length - 1;
+    var suffix = suffixes[order];
+    return CanvasJS.formatNumber(e.value / Math.pow(1000, order)) + suffix;
+  }
+  
+
+
+  ngOnInit(): void {
+ 
+    let dataPoints1 : any[] = [];
+    let dataPoints2 : any[] = [];
+    let dataPoints3 : any[] = [];
+    let dpsLength = 0;
+    let chart = new CanvasJS.StockChart("chartContainer",{
+      theme: "light2",
+      exportEnabled: true,
+      title:{
+        text:"Angular StockChart with Price & Volume"
+      },
+      subtitles: [{
+        text: "ETH/USD"
+      }],
+      charts: [{
+        toolTip: {
+          shared: true
+        },
+        axisX: {
+          lineThickness: 5,
+          tickLength: 0,
+          labelFormatter: function(e) {
+            return "";
+          },
+          crosshair: {
+            enabled: true,
+            snapToDataPoint: true,
+            labelFormatter: function(e) {
+              return "";
+            }
+          }
+        },
+        axisY: {
+          prefix: "$",
+          tickLength: 0,
+          title: "Etherium Price",
+        },
+        legend: {
+          verticalAlign: "top"
+        },
+        data: [{
+          name: "Price",
+          yValueFormatString: "$#,###.##",
+          xValueFormatString: "MMM DD YYYY",
+          type: "candlestick",
+          dataPoints : dataPoints1
+        }]
+      },{
+        height: 100,
+        toolTip: {
+          shared: true
+        },
+        axisX: {
+          crosshair: {
+            enabled: true,
+            snapToDataPoint: true,
+            valueFormatString: "MMM DD YYYY"
+          }
+        },
+        axisY: {
+          prefix: "$",
+          tickLength: 0,
+          title: "Volume",
+          labelFormatter: this.addSymbols
+        },
+        legend: {
+          verticalAlign: "top"
+        },
+        data: [{
+          name: "Volume",
+          yValueFormatString: "$#,###.##",
+          xValueFormatString: "MMM DD YYYY",
+          dataPoints : dataPoints2
+        }]
+      }],
+      navigator: {
+        data: [{
+          dataPoints: dataPoints3
+        }],
+        slider: {
+          minimum: new Date("2018-05-01"),
+          maximum: new Date("2018-07-01")
+        }
+      }
+    });
+    $.getJSON("https://canvasjs.com/data/docs/ethusd2018.json", function(data) {
+      for(var i = 0; i < data.length; i++){
+        dataPoints1.push({x: new Date(data[i].date), y: [Number(data[i].open), Number(data[i].high), Number(data[i].low), Number(data[i].close)]});;
+        dataPoints2.push({x: new Date(data[i].date), y: Number(data[i].volume_usd)});
+        dataPoints3.push({x: new Date(data[i].date), y: Number(data[i].close)});
+      }
+      chart.render();
+    });
+  }
+
+}
+
