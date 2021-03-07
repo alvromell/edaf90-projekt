d5d372d3137dbd453edb413fcb9c68f50fd28e7c
Jakob Westerberg <jaw@Jakobs-MacBook-Pro.local>
2021-03-08 00:42:14 +0100

    added real data to chart

--- my-app/src/app/stock-data.service.ts
+++ my-app/src/app/stock-data.service.ts
@@ -8,14 +8,32 @@ export class StockDataService {
 
   constructor() {}
 
-  getTimeSeries(stocks : string[], time : string, range : string) : any[] {
+  getStockData(responses : Response[]) : any[] {
+    let data : any = [];
+
+    responses.forEach((response : any) => {
+        console.log(response);
+        let stock : any = {};
+        stock.ticker = response.chart.result[0].meta.symbol;
+        stock.timeStamps = response.chart.result[0].timestamp;
+        stock.close = response.chart.result[0].indicators.quote[0].close;
+        stock.open = response.chart.result[0].indicators.quote[0].open;
+        stock.high = response.chart.result[0].indicators.quote[0].high;
+        stock.low = response.chart.result[0].indicators.quote[0].low;
+        stock.volume = response.chart.result[0].indicators.quote[0].volume;
+              
+        data.push(stock);
+    });
+    return data;
+  }
+
+  async getTimeSeries(stocks : string[], time : string, range : string) : Promise<Response[]> {
     /*
      * intervals:  '1m', '5m', '15m', '1d', '1wk', '1mo'
      * range:      '1d', '5d', '3mo', '6mo', '1y', '5y', '10y', 'ytd', 'max'
      */
 
     let proms : Promise<Response>[] = [];
-    let stockList : any[] = [];
 
     stocks.forEach(stock => fetchData(stock));
 
@@ -34,15 +52,21 @@ export class StockDataService {
       }));
     }
 
-    Promise.all(proms).then(list => list.forEach((response : any) => {
-      let stock : any = {};
-      stock.name = response.chart.result[0].meta.symbol;
-      stock.timestamp = response.chart.result[0].timestamp;
-      stock.currentPrice = response.chart.result[0].meta.regularMarketPrice;
-      stock.priceTimeSeries = response.chart.result[0].indicators.adjclose[0].adjclose;
-      stockList.push(stock);
+    /*
+    Promise.all(proms).then(list =>
+        list.forEach((response : any) => {
+          let stock : any = {};
+          stock.ticker = response.chart.result[0].meta.symbol;
+          stock.timeStamps = response.chart.result[0].timestamp;
+          stock.currentPrice = response.chart.result[0].meta.regularMarketPrice;
+          stock.priceTimeSeries = response.chart.result[0].indicators.adjclose[0].adjclose;
+                
+          stockList.push(stock);
+          //proms[0].then((response : any) => console.log(response.chart.result[0].meta.symbol));
     }));
+    */
+    let allProms : Promise<Response[]> = Promise.all(proms);
 
-    return stockList;
+    return allProms;
   }
 }
--- my-app/src/app/stock/stock.component.ts
+++ my-app/src/app/stock/stock.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 import * as CanvasJS from '../../assets/canvasjs-stock-1.2.10/canvasjs.stock.min';
 
+import { StockDataService } from '../stock-data.service'
+
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
@@ -9,7 +11,7 @@ import * as CanvasJS from '../../assets/canvasjs-stock-1.2.10/canvasjs.stock.min
 })
 export class StockComponent implements OnInit {
 
-  constructor() { }
+  constructor(private stockData : StockDataService) { }
   addSymbols(e){
     var suffixes = ["", "K", "M", "B"];
     var order = Math.max(Math.floor(Math.log(e.value) / Math.log(1000)), 0);
@@ -22,98 +24,114 @@ export class StockComponent implements OnInit {
 
 
   ngOnInit(): void {
- 
-    let dataPoints1 : any[] = [];
-    let dataPoints2 : any[] = [];
-    let dataPoints3 : any[] = [];
-    let dpsLength = 0;
-    let chart = new CanvasJS.StockChart("chartContainer",{
-      theme: "light2",
-      exportEnabled: true,
-      title:{
-        text:"Angular StockChart with Price & Volume"
-      },
-      subtitles: [{
-        text: "ETH/USD"
-      }],
-      charts: [{
-        toolTip: {
-          shared: true
+    let data : any[] = [];
+    let prom :  Promise<Response[]> = this.stockData.getTimeSeries(['AAPL'], '1d', '1y');
+    prom.then(responses => {data = this.stockData.getStockData(responses);}).then(_ => {
+      console.log(data[0].timeStamps[0]);
+      let dataPoints1 : any[] = [];
+      let dataPoints2 : any[] = [];
+      let dataPoints3 : any[] = [];
+      let dpsLength = 0;
+
+      for(let i = 0; i < data[0].timeStamps.length; i++) {
+        dataPoints1.push({x: new Date(1000 * data[0].timeStamps[i]), y: [Number(data[0].open[i]), Number(data[0].high[i]), Number(data[0].low[i]), Number(data[0].close[i])]});;
+        dataPoints2.push({x: new Date(1000 * data[0].timeStamps[i]), y: Number(data[0].volume[i])});
+        dataPoints3.push({x: new Date(1000 * data[0].timeStamps[i]), y: Number(data[0].close[i])});
+      }
+
+      let chart = new CanvasJS.StockChart("chartContainer",{
+        theme: "light2",
+        exportEnabled: true,
+        title:{
+          text:"Angular StockChart with Price & Volume"
         },
-        axisX: {
-          lineThickness: 5,
-          tickLength: 0,
-          labelFormatter: function(e) {
-            return "";
+        subtitles: [{
+          text: "ETH/USD"
+        }],
+        charts: [
+          {
+          toolTip: {
+            shared: true
           },
-          crosshair: {
-            enabled: true,
-            snapToDataPoint: true,
+          axisX: {
+            lineThickness: 5,
+            tickLength: 0,
             labelFormatter: function(e) {
               return "";
+            },
+            crosshair: {
+              enabled: true,
+              snapToDataPoint: true,
+              labelFormatter: function(e) {
+                return "";
+              }
             }
-          }
+          },
+          axisY: {
+            prefix: "$",
+            tickLength: 0,
+            title: "Etherium Price",
+          },
+          legend: {
+            verticalAlign: "top"
+          },
+          data: [{
+            name: "Price",
+            yValueFormatString: "$#,###.##",
+            xValueFormatString: "MMM DD YYYY",
+            type: "candlestick",
+            dataPoints : dataPoints1
+          }]
         },
-        axisY: {
-          prefix: "$",
-          tickLength: 0,
-          title: "Etherium Price",
-        },
-        legend: {
-          verticalAlign: "top"
-        },
-        data: [{
-          name: "Price",
-          yValueFormatString: "$#,###.##",
-          xValueFormatString: "MMM DD YYYY",
-          type: "candlestick",
-          dataPoints : dataPoints1
-        }]
-      },{
-        height: 100,
-        toolTip: {
-          shared: true
-        },
-        axisX: {
-          crosshair: {
-            enabled: true,
-            snapToDataPoint: true,
-            valueFormatString: "MMM DD YYYY"
-          }
-        },
-        axisY: {
-          prefix: "$",
-          tickLength: 0,
-          title: "Volume",
-          labelFormatter: this.addSymbols
-        },
-        legend: {
-          verticalAlign: "top"
-        },
-        data: [{
-          name: "Volume",
-          yValueFormatString: "$#,###.##",
-          xValueFormatString: "MMM DD YYYY",
-          dataPoints : dataPoints2
-        }]
-      }],
-      navigator: {
-        data: [{
-          dataPoints: dataPoints3
+        {
+          height: 100,
+          toolTip: {
+            shared: true
+          },
+          axisX: {
+            crosshair: {
+              enabled: true,
+              snapToDataPoint: true,
+              valueFormatString: "MMM DD YYYY"
+            }
+          },
+          axisY: {
+            prefix: "$",
+            tickLength: 0,
+            title: "Volume",
+            labelFormatter: this.addSymbols
+          },
+          legend: {
+            verticalAlign: "top"
+          },
+          data: [{
+            name: "Volume",
+            yValueFormatString: "$#,###.##",
+            xValueFormatString: "MMM DD YYYY",
+            dataPoints : dataPoints2
+          }]
         }],
-        slider: {
-          minimum: new Date("2018-05-01"),
-          maximum: new Date("2018-07-01")
+        navigator: {
+          data: [{
+            dataPoints: dataPoints3
+          }],
+          slider: {
+            //minimum: new Date(data[0].timeStamps[0]),
+            //maximum: new Date(data[0].timeStamps[data[0].timeStamps.length - 1])
+          }
         }
-      }
-    });
-    $.getJSON("https://canvasjs.com/data/docs/ethusd2018.json", function(data) {
-      for(var i = 0; i < data.length; i++){
-        dataPoints1.push({x: new Date(data[i].date), y: [Number(data[i].open), Number(data[i].high), Number(data[i].low), Number(data[i].close)]});;
-        dataPoints2.push({x: new Date(data[i].date), y: Number(data[i].volume_usd)});
-        dataPoints3.push({x: new Date(data[i].date), y: Number(data[i].close)});
-      }
+      });
       chart.render();
+      /*
+      $.getJSON("https://canvasjs.com/data/docs/ethusd2018.json", function(data) {
+        for(var i = 0; i < data.length; i++){
+          dataPoints1.push({x: new Date(data[i].date), y: [Number(data[i].open), Number(data[i].high), Number(data[i].low), Number(data[i].close)]});;
+          dataPoints2.push({x: new Date(data[i].date), y: Number(data[i].volume_usd)});
+          dataPoints3.push({x: new Date(data[i].date), y: Number(data[i].close)});
+        }
+        chart.render();
+      });
+      */
     });
   }
 
