06aa835886649e578218640de9234e4146779e8e
Alv Romell <alvromell@gmail.com>
2021-03-12 09:39:34 +0100

    Lagrar data för daglig förändring

--- my-app/src/app/dashboard/dashboard.component.ts
+++ my-app/src/app/dashboard/dashboard.component.ts
@@ -11,25 +11,44 @@ export class DashboardComponent implements OnInit {
 
   stocks: any;
   stockData: any;
+  dailyChange: any;
 
   constructor(private stockDataService : StockDataService) { 
 
-    this.stockData = this.stockDataService.getStockData();
-    console.log(this.stockData);
-    
-
     console.log(stockList)
-    this.stocks = stockList.reduce((acc, curr) => {
-      console.log(acc, curr.name, window.localStorage.getItem(curr.name));
+    this.stocks = stockList.reduce((acc: any, curr: { name: string; key: any; }) => {
+    //  console.log(acc, curr.name, window.localStorage.getItem(curr.name));
       if(window.localStorage.getItem(curr.name) === "true")
         return [...acc, {name: curr.name, key:curr.key}];
       else 
         return acc
     }, [])
     console.log(this.stocks)
+    
+    let stocksToFetch = this.stocks.map((elem: { key: any; }) => elem.key);
+    let prom = this.stockDataService.fetchData(stocksToFetch,'1d','1y');
+    prom.then( _ => this.stockData = this.stockDataService.getStockData())
+    .then( _ => this.dailyChange = this.getDailyChange());
+    
   }
 
   ngOnInit(): void {
+    
+  }
+
+  getDailyChange() {
+    
+    let dailyChanges = this.stockData.map((stock) =>
+      ({key: stock.ticker, 
+        change: Math.round(((stock.close[stock.close.length-1] - stock.open[stock.open.length-1])/stock.open[stock.open.length-1]) * 10000) / 100,
+        open: Math.round(stock.open[stock.open.length-1] * 100)/100,
+        close: Math.round(stock.close[stock.close.length-1] * 100)/100
+      }) 
+    );
+    console.log(this.stockData)
+    console.log(dailyChanges);
+    
+    return dailyChanges;
   }
 
 }
